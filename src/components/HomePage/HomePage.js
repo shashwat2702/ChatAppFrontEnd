17ce10b86aaa23a4541a45a5521e157a0f1f65fb
Shashwat Sinha <shashwat_sinha@external.mckinsey.com>
2019-05-22 00:32:52 +0530

    Implemented functionality to send the msgs to server

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,20 +1,76 @@
-import React, { Component, Fragment} from 'react';
+import React, { Component, Fragment } from 'react';
 import { withRouter } from 'react-router-dom';
+import io from "socket.io-client";
 import  './HomePage.scss';
+import Input from '../shared/Input/Input';
+import Button from '../shared/Button/Button';
 
 
 class HomePage extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      message: ''
+    }
+    this.socket = io('localhost:8080');
+    this.socket.on('RECEIVE_MESSAGE', function(data){
+      console.log(data);
+});
+  }
   componentDidMount() {
     const { isLoggedIn, history } = this.props;
     if(!isLoggedIn){
       history.push('/');
     }
   }
+  handleInputChange = (event) => {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value
+    });
+  }
+  sendMessage = (event) => {
+    event.preventDefault();
+    const { username } = this.props;
+    this.socket.emit('SEND_MESSAGE', {
+        author: username,
+        message: this.state.message
+    });
+    this.setState({message: ''});
+  };
   render() {
+    const { message } = this.state;
     return (
-    <Fragment> 
-      <p> Secure Page</p>
-    </Fragment>
+      <Fragment>
+        <div className="homePageConatiner">
+          <div className="activeUsers">
+            List Of Participants
+          </div>
+          <div className="actualMessages"> 
+            <div className="receivedMessages">
+            Received Msgs go here
+            </div>
+            <div className="sendMessageContainer">
+              <div className="messageInput">
+                <Input
+                  id={'textForSending'}
+                  type={'text'}
+                  name={'message'}
+                  placeholder={'Type your messages here'}
+                  value={message}
+                  onChange={this.handleInputChange}
+                  autoComplete={'off'}
+                />
+              </div>
+              <div className="sendButton">
+                <Button className={'sendButton button'} type="submit" label={'Send'} onClick={this.sendMessage}/>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Fragment>
     )
   }
 };
