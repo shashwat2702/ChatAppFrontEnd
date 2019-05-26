91b7fbc0ed4ca92d4341ade829fe1473f10f89c6
Shashwat Sinha <shashwatsinha2702@gmail.com>
2019-05-27 03:09:39 +0530

    Added proptypes validation
    * Removed multiple message bug
    
    * Enforced ESLint across app

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,78 +1,97 @@
+/* eslint-disable react/forbid-prop-types */
 import React, { Component, Fragment } from 'react';
 import { withRouter } from 'react-router-dom';
-import io from "socket.io-client";
-import  './HomePage.scss';
+import io from 'socket.io-client';
+import PropTypes from 'prop-types';
+import './HomePage.scss';
 import Input from '../shared/Input/Input';
 import Button from '../shared/Button/Button';
 
 
 class HomePage extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       message: '',
-      allActiveUsers: []
-    }
+      allActiveUsers: [],
+      username: props.username,
+    };
     this.socket = io('localhost:8080');
+    const { username } = this.state;
     this.socket.emit('NEW USER', {
-        username: props.username
+      username,
     });
     this.socket.on('LIST OF ACTIVE USERS', (data) => {
-        this.setState({allActiveUsers: data});
+      this.setState({ allActiveUsers: data });
     });
-    this.socket.on('RECEIVE_MESSAGE', function(data){
+    this.socket.on('RECEIVE_MESSAGE', (data) => {
       props.addMessage(data.author, data.message);
     });
   }
+
   componentDidMount() {
     const { isLoggedIn, history } = this.props;
-    if(!isLoggedIn){
+    if (!isLoggedIn) {
       history.push('/');
     }
   }
+
   componentDidUpdate(prevProps) {
     const { isLoggedIn, history } = this.props;
     if (isLoggedIn !== prevProps.isLoggedIn) {
       history.push('/');
     }
-}
+  }
+
   componentWillUnmount() {
+    const { username } = this.state;
     this.socket.emit('USER DISCONNECTED', {
-        username: this.props.username
+      username,
     });
+    this.socket.off();
   }
+
   handleInputChange = (event) => {
-    const target = event.target;
-    const value = target.value;
-    const name = target.name;
+    const { target } = event;
+    const { value } = target;
+    const { name } = target;
     this.setState({
-      [name]: value
+      [name]: value,
     });
   }
+
   sendMessage = (event) => {
     event.preventDefault();
     const { username } = this.props;
+    const { message } = this.state;
     this.socket.emit('SEND_MESSAGE', {
-        author: username,
-        message: this.state.message
+      author: username,
+      message,
     });
-    this.setState({message: ''});
+    this.setState({ message: '' });
   };
+
   showAllMessages = () => {
-    const { messages} = this.props;
-    return messages.map((messageItem, index) => {
-        return(<div> {messageItem.sender} : {messageItem.message}</div>)
-    });
+    const { messages } = this.props;
+    return messages.map(messageItem => (
+      <div>
+        {' '}
+        {messageItem.sender}
+        {' '}
+:
+        {' '}
+        {messageItem.message}
+      </div>
+    ));
   }
+
   showAllActiveUsers = () => {
     const { allActiveUsers } = this.state;
-    return allActiveUsers.map((user) => {
-      return(<div>{user}</div>)
-    })
+    return allActiveUsers.map(user => (<div>{user}</div>));
   }
+
   render() {
     const { message } = this.state;
-    console.log(this.props);
     return (
       <Fragment>
         <div className="homePageConatiner">
@@ -80,30 +99,37 @@ class HomePage extends Component {
             List Of Participants
             {this.showAllActiveUsers()}
           </div>
-          <div className="actualMessages"> 
+          <div className="actualMessages">
             <div className="receivedMessages">
-            {this.showAllMessages()}
+              {this.showAllMessages()}
             </div>
-            <form className="sendMessageContainer" onSubmit={ (e) => { e.preventDefault(); } }>
+            <form className="sendMessageContainer" onSubmit={(e) => { e.preventDefault(); }}>
               <div className="messageInput">
                 <Input
-                  id={'textForSending'}
-                  type={'text'}
-                  name={'message'}
-                  placeholder={'Type your messages here'}
+                  id="textForSending"
+                  type="text"
+                  name="message"
+                  placeholder="Type your messages here"
                   value={message}
                   onChange={this.handleInputChange}
-                  autoComplete={'off'}
+                  autoComplete="off"
                 />
               </div>
               <div className="sendButton">
-                <Button className={'sendButton button'} type="submit" label={'Send'} onClick={this.sendMessage}/>
+                <Button className="sendButton button" type="submit" label="Send" onClick={this.sendMessage} />
               </div>
             </form>
           </div>
         </div>
       </Fragment>
-    )
+    );
   }
+}
+HomePage.propTypes = {
+  username: PropTypes.string.isRequired,
+  isLoggedIn: PropTypes.bool.isRequired,
+  addMessage: PropTypes.func.isRequired,
+  history: PropTypes.object.isRequired,
+  messages: PropTypes.array.isRequired,
 };
 export default withRouter(HomePage);
